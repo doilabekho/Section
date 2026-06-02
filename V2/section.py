1c793440197070bc766d17dae8bd64fbcb3cac1e
doilabekho <122877085+doilabekho@users.noreply.github.com>
2026-06-02 15:28:47 +0200

    Modify root function calls in section.py
    Uncommented the root function call with 'hybr' method and commented out the 'krylov' method call.

--- V2/section.py
+++ V2/section.py
@@ -1164,14 +1164,14 @@ def solve_GG_ELU_pararect(polygon1, evi, p_acier, s_acier,
             dh = np.zeros(3); dh[i] = h
             J[:,i] = (residuals(eps+dh) - r0) / h
         return J
-    #result = root(residuals, x0, jac=jacobian, method='hybr',
-    #              tol=1e-6, options={'maxfev': 1000})
+    result = root(residuals, x0, jac=jacobian, method='hybr',
+                  tol=1e-6, options={'maxfev': 1000})
     #result = root(residuals, x0, method='df-sane',
     #          tol=1e-6, options={'maxfev': 2000, 'fatol': 1e-6})
     #from scipy.optimize import fsolve
     #sol = fsolve(residuals, x0, xtol=1e-6)
-    result = root(residuals, x0, method='krylov',
-              tol=1e-5, options={'maxiter': 800})
+    #result = root(residuals, x0, method='krylov',
+    #          tol=1e-5, options={'maxiter': 800})
     return result.x
 
 
